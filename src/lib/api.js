71bc4ef47337722f80b859f54de2603e69f45e0a
jitendra-sudo <jitusaini2705@gmail.com>
2026-04-13 12:09:42 +0530

    sdfghgfdsa

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_BASE = "http://localhost:5000";
+export const API_BASE = "http://api.mentormeritsacademy.in";
 
 const api = axios.create({
   baseURL: API_BASE,
